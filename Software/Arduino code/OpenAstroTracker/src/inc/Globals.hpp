e3d2d895d4cb92635e782666365577b74b7bcc93
Andre Stefanov <mail@andrestefanov.de>
2020-12-06 21:47:41 +0100

    fixed trackign speed calculation
    Former-commit-id: b910356907ded573976948f4664c4a1277255007

---
+++ Software/Arduino code/OpenAstroTracker/src/inc/Globals.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+// TODO (AS): This is just a tmp fix, we need to get rid of these globals
+
+extern float RAStepsPerDegree;
+extern float DECStepsPerDegree;
+
+extern bool inSerialControl;
