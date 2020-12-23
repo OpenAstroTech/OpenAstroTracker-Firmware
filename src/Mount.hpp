aecc6677c717ca0fb97e965984a904a373b938ea
Philip Mair <philip@pmneo.de>
2020-12-23 12:53:59 +0100

    conditional connectToDriver definition

--- src/Mount.hpp
+++ src/Mount.hpp
@@ -325,7 +325,9 @@ private:
     String mountStatusString();
   #endif
 
+  #if RA_DRIVER_TMC2209_UART_MODE == TMC2209_MODE_UART || DEC_DRIVER_TMC2209_UART_MODE == TMC2209_MODE_UART 
   void connectToDriver( TMC2209Stepper* driver, const char *driverKind );
+  #endif
 
 private:
   LcdMenu* _lcdMenu;
