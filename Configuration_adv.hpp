bae6dfb0970fc76dae0236c41d7a8cc8698fa1de
Cameron Tetford <cameron@tetford.me>
2021-01-14 15:36:22 -0500

    Update Configuration_adv.hpp

--- Configuration_adv.hpp
+++ Configuration_adv.hpp
@@ -359,3 +359,27 @@
   #endif
 #endif
 
+////////////////////////////
+//
+// DEBUG OUTPUT
+//
+#define DEBUG_LEVEL (DEBUG_ANY)
+// #define DEBUG_LEVEL (DEBUG_STEPPERS|DEBUG_MOUNT)
+// #define DEBUG_LEVEL (DEBUG_INFO|DEBUG_MOUNT|DEBUG_GENERAL)
+// #define DEBUG_LEVEL (DEBUG_SERIAL|DEBUG_WIFI|DEBUG_INFO|DEBUG_MOUNT|DEBUG_GENERAL)
+// #define DEBUG_LEVEL (DEBUG_ANY)
+// #define DEBUG_LEVEL (DEBUG_INFO|DEBUG_MOUNT|DEBUG_GENERAL)
+//
+// Bit Name                 Output
+//  0  DEBUG_INFO           General output, like startup variables and status
+//  1  DEBUG_SERIAL         Serial commands and replies
+//  2  DEBUG_WIFI           Wifi related output
+//  3  DEBUG_MOUNT          Mount processing output
+//  4  DEBUG_MOUNT_VERBOSE  Verbose mount processing (coordinates, etc)
+//  5  DEBUG_GENERAL        Other misc. output
+//  6  DEBUG_MEADE          Meade command handling output
+// Set this to specify the amount of debug output OAT should send to the serial port.
+// Note that if you use an app to control OAT, ANY debug output will likely confuse that app.
+// Debug output is useful if you are using Wifi to control the OAT or if you are issuing
+// manual commands via a terminal.
+//
