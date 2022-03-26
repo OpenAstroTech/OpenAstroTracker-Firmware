eab78794601d073b1c3e45dcf55ec8f1db1e6716
Julianne Swinoga <julianneswinoga@gmail.com>
2022-03-26 18:47:50 -0400

    fixup! Update (#163)
    Replace BOARD_AVR_MEGA2560 with RAMPS in LocalConfiguration.hpp

--- LocalConfiguration.hpp
+++ LocalConfiguration.hpp
@@ -6,8 +6,8 @@
 // Don't load local configuration file
 #elif defined(MATRIX_LOCAL_CONFIG)
     #include "Configuration_local_matrix.hpp"
-#elif BOARD == BOARD_AVR_MEGA2560 && __has_include("Configuration_local_mega2560.hpp")
-    #include "Configuration_local_mega2560.hpp"
+#elif BOARD == BOARD_AVR_RAMPS && __has_include("Configuration_local_ramps.hpp")
+    #include "Configuration_local_ramps.hpp"
 #elif BOARD == BOARD_AVR_MKS_GEN_L_V21 && __has_include("Configuration_local_mksgenlv21.hpp")
     #include "Configuration_local_mksgenlv21.hpp"
 #elif BOARD == BOARD_AVR_MKS_GEN_L_V2 && __has_include("Configuration_local_mksgenlv2.hpp")
