ca7399de5637e10b820330dfb5b9c2ece262e1cc
Andre Stefanov <mail@andrestefanov.de>
2022-12-03 22:52:08 +0100

    using correct time base for hours check

--- src/DayTime.cpp
+++ src/DayTime.cpp
@@ -153,14 +153,14 @@ void DayTime::addHours(float deltaHours)
 
 void DayTime::checkHours()
 {
-    while (totalSeconds >= SIDEREAL_SECONDS_PER_DAY)
+    while (totalSeconds >= secondsPerDay)
     {
-        totalSeconds -= SIDEREAL_SECONDS_PER_DAY;
+        totalSeconds -= secondsPerDay;
     }
 
     while (totalSeconds < 0)
     {
-        totalSeconds += SIDEREAL_SECONDS_PER_DAY;
+        totalSeconds += secondsPerDay;
     }
 }
 
