c484979cbf78a420e85de052034dfdb019e312a5
Andre Stefanov <mail@andrestefanov.de>
2026-05-14 00:20:16 +0200

    chore: remove unused coverage_html.sh and format.sh scripts

--- scripts/format.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env sh
-
-find . \( -path './.git' -o -path './.pio' -o -path './build_cache' -o -path './src/libs/TimerInterrupt' \) -prune -o -type f \( -name '*.c' -o -name '*.cpp' -o -name '*.h' -o -name '*.hpp' \) -exec clang-format -i {} +
