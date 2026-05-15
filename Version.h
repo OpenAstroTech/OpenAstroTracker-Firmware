eea3cb285f741980c289b765df493ec0135b4f22
Andre Stefanov <mail@andrestefanov.de>
2026-05-15 22:03:28 +0200

    ci: Clean up comments in Version.h for clarity and add marker for release-please

--- Version.h
+++ Version.h
@@ -1,6 +1,3 @@
-// Version number must have only two digits maximum at each of the three locations.
-// So 1.8.99 is ok, but 1.8.234 is not. Neither is 1.123.22
-// Also, numbers are interpreted as simple numbers.                        _   __   _
-// So 1.8 is actually 1.08, meaning that 1.12 is a later version than 1.8.  \_(..)_/
+// Updated automatically by release-please-action in CI/CD pipeline. Do not edit manually.
 
-#define VERSION "v1.13.20"
+#define VERSION "v1.13.20"  // x-release-please-version
